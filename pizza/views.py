ac6742084a835313f37ad510577420fba7a64eb3
annoyingorangen <fatimaaliforwork@gmail.com>
2022-05-25 16:13:05 +0500

    Adds note as resposne when form data is submitted

--- pizza/views.py
+++ pizza/views.py
@@ -4,5 +4,14 @@ def home(request):
     return render(request,'pizza/home.html')
 
 def order(request):
-    form= PizzaForm()
-    return render(request,'pizza/order.html',{'form':form})
+    if request.method=='POST':
+        form_data=PizzaForm(request.POST)
+        if form_data.is_valid():
+            note=f"Thanks for ordering! Your {form_data.cleaned_data['size']} pizz is on its way"
+            form= PizzaForm()
+            return render(request,'pizza/order.html',{'form':form,'note':note})
+
+
+    else:
+        form= PizzaForm()
+        return render(request,'pizza/order.html',{'form':form})
